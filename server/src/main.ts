2731fded606b443e6ea8418716eaa86375b6911f
Umair Ahsan <umairahsan2019@gmail.com>
2026-02-13 12:13:53 +0500

    created files for prisma 7.4 setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './common/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
